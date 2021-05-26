00cd6a34d408ee7a64c41d2849298e84ad67790b
Moht Jangir <Mohit@M0hit>
2021-05-26 16:34:50 +0530

    noting special

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Form from "./components/Form";
+import Time from "./components/Time";
 function App() {
   const isLoggedIn = false;
   return (
     <div className="App">
       <Header />
+      <Time />
       {isLoggedIn === true ? <h1>Hello Mohit</h1> : <Form />}
       <Footer />
     </div>
---
+++ src/components/Time.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function Time() {
+  const statetime = new Date().toLocaleString();
+  const [time, setTime] = useState(statetime);
+
+  const updateTime = () => {
+    const newTime = new Date().toLocaleString();
+
+    setTime(newTime);
+
+    setInterval(updateTime, 1000);
+  };
+  return (
+    <div>
+      <h1>Time</h1>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
+    </div>
+  );
+}
+export default Time;
